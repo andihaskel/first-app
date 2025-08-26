56f300092ab2ed0163b1a4075903b263c5a85d44
Andrés Haskel <andres.haskel@gmail.com>
2025-08-25 21:49:22 -0300

    Updated TodayScreen.tsx

--- app/screens/TodayScreen.tsx
+++ app/screens/TodayScreen.tsx
@@ -396,7 +396,7 @@ const styles = StyleSheet.create({
   flexDirection: 'row',
   justifyContent: 'space-between',
   alignItems: 'center',
-  marginTop: 12,  
+  marginTop: 10,  
 },
   dropdownText: { fontSize: 15, color: '#374151' },
   sendButton: { width: 44, height: 44, borderRadius: 22, backgroundColor: '#f44336', justifyContent: 'center', alignItems: 'center' },
